fac6efd7ed118faf0ed22ecb3e78b1ad709412f7
Thong Dang <thongdn@nustechnology.com>
2015-01-29 11:59:52 +0700

    29/01/2015 CoreData

--- build/Demo.build/Debug-iphonesimulator/Demo.build/Objects-normal/i386/LoginViewController.d
+++ build/Demo.build/Debug-iphonesimulator/Demo.build/Objects-normal/i386/LoginViewController.d
@@ -3,4 +3,5 @@ dependencies: \
   /Users/hackintosh01/Documents/NgocThong/TaiLieu/laptrinh/Code/KnodeitModuleTester_iOS/KnodeitModuleTester/Modules/Thong_Login/LoginViewController.h \
   /Users/hackintosh01/Documents/NgocThong/TaiLieu/laptrinh/Code/KnodeitModuleTester_iOS/KnodeitModuleTester/Utilities/Categories/NSString/NSString+Validator.h \
   /Users/hackintosh01/Documents/NgocThong/TaiLieu/laptrinh/Code/KnodeitModuleTester_iOS/KnodeitModuleTester/Modules/Thong_List/ListViewController.h \
+  /Users/hackintosh01/Documents/NgocThong/TaiLieu/laptrinh/Code/KnodeitModuleTester_iOS/KnodeitModuleTester/Modules/Thong_Detail/DetailViewController.h \
   /Users/hackintosh01/Documents/NgocThong/TaiLieu/laptrinh/Code/KnodeitModuleTester_iOS/KnodeitModuleTester/Model/Data/Data_Text.h
